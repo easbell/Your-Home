038bfd225bc90247326008807fe9e29de13be89e
Elizabeth Asbell <e.asbell8@gmail.com>
2019-05-19 19:02:18 -0600

    Implement antd modal form.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import NewProject from '../NewProject/NewProject';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>YourHome</h1>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App" id='App'>
+        <header className="App-header">
+          <h1>YourHome</h1>
+          <NewProject />
+        </header>
+      </div>
+    );
+  }
+} 
 
 export default App;
---
+++ src/containers/NewProject/NewProject.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Button, Modal, Form, Input } from 'antd';
+import './NewProject.css';
+
+const ProjectForm = Form.create({ name: 'form_in_modal' })(
+  class extends React.Component {
+    render() {
+      const { visible, onCancel, onCreate, form } = this.props;
+      const { getFieldDecorator } = form;
+      return (
+        <Modal
+          visible={visible}
+          title="Create a new project"
+          okText="Create"
+          onCancel={onCancel}
+          onOk={onCreate}
+        >
+          <Form layout="vertical">
+            <Form.Item label="Name">
+              {getFieldDecorator('name', {
+                rules: [{ required: true, message: 'Please input the name of the project!' }],
+              })(<Input />)}
+            </Form.Item>
+            <Form.Item label="Description">
+              {getFieldDecorator('description')(<Input type="textarea" />)}
+            </Form.Item>
+          </Form>
+        </Modal>
+      );
+    }
+  },
+);
+
+class NewProject extends React.Component {
+  state = {
+    visible: false,
+  };
+
+  showModal = () => {
+    this.setState({ visible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+
+      console.log('Received values of form: ', values);
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+  };
+
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
+
+  render() {
+    return (
+      <div>
+        <Button 
+          type="primary" 
+          id='add-project'
+          onClick={this.showModal}>
+            New Project
+        </Button>
+        <ProjectForm
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}
+        />
+      </div>
+    );
+  }
+}
+
+export default NewProject;
