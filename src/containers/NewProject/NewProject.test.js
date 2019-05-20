c12ebcba3a2805d785197d463252bd13b564d34d
Elizabeth Asbell <e.asbell8@gmail.com>
2019-05-20 16:03:33 -0600

    Add in NewProject tests.

--- src/containers/NewProject/NewProject.test.js
+++ src/containers/NewProject/NewProject.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NewProject from './NewProject';
+import ProjectForm from './NewProject';
 
 describe('App', () => {
   let wrapper;
@@ -11,5 +12,20 @@ describe('App', () => {
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should setState on handleCancel', () => {
+    wrapper.instance().handleCancel();
+    expect(wrapper.state('visible')).toBe(false);
+  });
+
+  it('should setState on showModal', () => {
+    wrapper.instance().showModal();
+    expect(wrapper.state('visible')).toBe(true);
+  });
+
+  it('should setState on handleCreate', () => {
+    wrapper.instance().handleCreate();
+    expect(wrapper.state('visible')).toBe(false);
   });
 });
