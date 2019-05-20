d64b8e94d9d928c9b4e276b8641d1322cac1f408
Elizabeth Asbell <e.asbell8@gmail.com>
2019-05-20 15:06:14 -0600

    Add test structure.

---
+++ src/mockProject.js
@@ -0,0 +1,62 @@
+const mockProject = {
+  "name": "My Project",
+  "description": "description",
+  "address": "2285 S Bannock",
+  "city": "Denver",
+  "state": "CO",
+  "zip_Code": "80223",
+  "start_Date": "date",
+  "end_Date": "date",
+  "rooms": [
+    {
+      "name": "kitchen",
+      "type": "kitchen",
+      "materials": [
+        {
+          "element_type": "sink",
+          "name": "white sink",
+          "model_number": "1bojdf03",
+          "brand": "Kohler",
+          "vendor": "Home Depot",
+          "manual_url": "link",
+          "notes": "notes"
+        },
+        {
+          "element_type": "fridge",
+          "name": "fridge",
+          "model_number": "1bojdf03",
+          "brand": "Frigidaire",
+          "vendor": "Home Depot",
+          "manual_url": "link",
+          "notes": "notes"
+        }
+      ]
+    },
+    {
+      "name": "bathroom",
+      "type": "bathroom",
+      "materials": [
+        {
+          "element_type": "sink",
+          "name": "white sink",
+          "model_number": "1bojdf03",
+          "brand": "Kohler",
+          "vendor": "Home Depot",
+          "manual_url": "link",
+          "notes": "notes"
+        },
+        {
+          "element_type": "toilet",
+          "name": "toilet",
+          "model_number": "1bojdf03",
+          "brand": "",
+          "vendor": "Home Depot",
+          "manual_url": "link",
+          "notes": "notes"
+        }
+      ]
+    }
+  ]
+}
+
+export default mockProject;
