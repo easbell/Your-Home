ad4ae891be63fffd4bdc24cc99107e36acd0f084
Mason France <masonrf1@gmail.com>
2019-05-20 16:19:37 -0600

    Initialize Sass for styling

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import NewProject from '../NewProject/NewProject';
 
 class App extends Component {
--- src/containers/NewProject/NewProject.js
+++ src/containers/NewProject/NewProject.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button, Modal, Form, Input } from 'antd';
-import './NewProject.css';
 
 const ProjectForm = Form.create({ name: 'form_in_modal' })(
   class extends React.Component {
