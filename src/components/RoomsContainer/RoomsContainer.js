ad711570349e63b6229b3db36baa1717ac5b7615
Mason France <masonrf1@gmail.com>
2019-05-21 16:54:44 -0600

    Link components from ProjectDetails to RoomsContainer to Room

---
+++ src/components/RoomsContainer/RoomsContainer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Room from '../../containers/Room/Room'
+
+export class RoomsContainer extends Component {
+  
+  // renderRooms = () => {
+  //   return mockProjects.data.map((room, i) => {
+  //     return <Room key={i} {...room}/>
+  //   });
+  // }
+
+  render() {
+    return (
+      <div className='rooms'>
+        <Room />
+        {/* {this.renderRooms()} */}
+      </div>
+    )
+  }
+}
+
+export default RoomsContainer
--- src/containers/ProjectDetails/ProjectDetails.js
+++ src/containers/ProjectDetails/ProjectDetails.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import RoomsContainer from '../../components/RoomsContainer/RoomsContainer'
 
 class ProjectDetails extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
-    console.log(this.props)
     return (
-      <p>Hello</p>
+      <div>
+        <div className='project-details'>
+          <h2>Project Name</h2>
+          <p>Details gathered from props</p>
+        </div>
+        <RoomsContainer />
+      </div>
     )
   }
 }
