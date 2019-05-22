c92e27eac7e45b6bb8623e2547b8def94aa3eb47
Mason France <masonrf1@gmail.com>
2019-05-22 15:10:39 -0600

    Make each room able to open a side drawer

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,21 +1,61 @@
 import React from 'react';
-import { Collapse } from 'antd';
+import { Drawer, List } from 'antd';
 import RoomItems from '../../containers/RoomItems/RoomItems'
 
-const Panel = Collapse.Panel;
+class Room extends React.Component {
+  state = { visible: false };
 
-function callback(key) {
-  console.log(key);
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+ // make method to dynamically make items in the dataSource array within List
+
+  render() {
+    return (
+      <div>
+        <List
+          dataSource={[
+            {
+              name: 'Bedroom',
+              description: 'Upstairs bedroom',
+            },
+            {
+              name: 'Kitchen',
+              description: 'Full kitchen',
+            },
+          ]}
+          bordered
+          renderItem={item => (
+            <List.Item key={item.id} actions={[<a onClick={this.showDrawer}>View Materials</a>]}>
+              <List.Item.Meta
+                title={item.name}
+                description={item.description}
+              />
+            </List.Item>
+          )}
+        />
+        <Drawer
+          width={640}
+          placement="right"
+          closable={false}
+          onClose={this.onClose}
+          visible={this.state.visible}
+        >
+          <RoomItems />
+        </Drawer>
+      </div>
+    );
+  }
 }
 
-const Room = ({projId, roomId}) => {
-  return (
-    <Collapse onChange={callback}>
-      <Panel header="BedRoom" key="1">
-        <RoomItems />
-      </Panel>
-    </Collapse>
-  )
-}
 
 export default Room;
