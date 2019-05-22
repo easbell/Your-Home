ac788ef67791dd8ef7ce5558b028260ff8d6d404
Mason France <masonrf1@gmail.com>
2019-05-21 21:17:39 -0600

    Add simplified route for purposes of eae of navigation while building UI

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -15,9 +15,9 @@ class App extends Component {
   }
 
   renderRoomItems = ({ match }) => {
-    const project = mockProjects.data.find(project => parseInt(project.id) === match.params.id)
-    const room = project.rooms.find(room => parseInt(room.id) === match.params.id)
-    return <RoomItems room={room} />
+    // const project = mockProjects.data.find(project => parseInt(project.id) === match.params.id)
+    // const room = project.rooms.find(room => parseInt(room.id) === match.params.id)
+    return <RoomItems />
   }
 
   render() {
