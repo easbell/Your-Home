9dd68d694b83a8d2ad0bd327c585e1c8e20b8131
Mason France <masonrf1@gmail.com>
2019-05-26 11:19:12 -0600

    Make EditMaterial a modal form

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Drawer, List } from 'antd';
-import RoomItems from '../../containers/RoomItems/RoomItems'
+import RoomItems from '../../containers/RoomItems/RoomItems';
+import EditRoom from '../../containers/EditRoom/EditRoom';
 
 class Room extends React.Component {
   state = { visible: false };
@@ -40,7 +41,8 @@ class Room extends React.Component {
           renderItem={item => (
             <List.Item key={item.id} actions={[
               <a onClick={this.showDrawer}>View Materials</a>,
-              <a onClick={this.editRoom}><i className="fas fa-pen"></i></a>,
+              // <a onClick={this.editRoom}><i className="fas fa-pen"></i></a>,
+              <EditRoom />,
               <a onClick={this.deleteRoom}><i className="fas fa-trash-alt"></i></a>
             ]}>
               <List.Item.Meta
