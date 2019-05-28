b84a8bc9f2e4412e0df2150ca3b6c1de04135628
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-05-28 12:14:41 -0600

    Fetching projects from api

--- src/index.js
+++ src/index.js
@@ -9,6 +9,22 @@ import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import rootReducer from './reducers';
 
+export const addProject = async () => {
+  const url = "https://hometrackr.herokuapp.com//api/v1/graphql"
+  const response = await fetch(url, {
+    body: JSON.stringify({ "query":"mutation { createProject (project: {name: \"My House name\", description: \"made by graphql\", address: \"some address\"}) {id name description address }}"
+  }),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  const data = response.json()
+  console.log(data)
+ }
+
+addProject()
+
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
