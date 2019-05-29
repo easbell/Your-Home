f0ca7093f3f9b00a91f0a887d2989f1fd8240537
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-05-29 13:09:33 -0600

    Deleteconfirm testing

--- src/components/DeleteConfirm/DeleteConfirm.test.js
+++ src/components/DeleteConfirm/DeleteConfirm.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import DeleteConfirm from './DeleteConfirm';
+import {DeleteConfirm} from './DeleteConfirm';
 
 describe('DeleteConfirm', () => {
   let wrapper;
+  let props = {
+    id: 3,
+    deleteProject: jest.fn()
+  }
   beforeEach(() => {
-    wrapper = shallow(<DeleteConfirm type='project'/>)
+    wrapper = shallow(<DeleteConfirm {...props}/>)
   });
 
   it('should match snapshot', () => {
@@ -13,14 +17,13 @@ describe('DeleteConfirm', () => {
   });
 
   it.skip('should run showConfirm when button is clicked', () => {
-    wrapper.find('.confirm-btn').simulate('click', wrapper.props.type);
+    wrapper.find('.confirm-btn').simulate('click', props);
     const spy = jest.spyOn(wrapper.instance(), 'showConfirm');
     expect(spy).toHaveBeenCalled();
   });
 
-  it('should confirm new data', () => {
-    // prob check to see if correct functions are run depending on type
-    //
+  it('should delete a project when OK is clicked', () => {
+    wrapper.instance().showConfirm()
   });
 
   //not sure if we need to test cancel?
